df3ceb6b3933851c15a322b262853b5cb31f96f6
joelsantos-js <joeltere9@gmail.com>
2024-02-22 14:51:10 -0300

    Initial commit

---
+++ src/main/java/com/joel/br/JJ/TECH/JjTechApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.JJ.TECH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/JJ/TECH/JjTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.JJ.TECH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
